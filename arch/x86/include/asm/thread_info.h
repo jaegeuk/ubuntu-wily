5f54bebd73a3903514d7ab786085b21ea757f92b
Andy Lutomirski <luto@kernel.org>
2015-07-10 11:35:31 -0700

    x86/nmi/64: Switch stacks on userspace NMI entry
    commit 9b6e6a8334d56354853f9c255d1395c2ba570e0a upstream
    
    Returning to userspace is tricky: IRET can fail, and ESPFIX can
    rearrange the stack prior to IRET.
    
    The NMI nesting fixup relies on a precise stack layout and atomic
    IRET.  Rather than trying to teach the NMI nesting fixup to handle
    ESPFIX and failed IRET, punt: run NMIs that came from user mode on
    the normal kernel stack.
    
    This will make some nested NMIs visible to C code, but the C code is
    okay with that.
    
    As a side effect, this should speed up perf: it eliminates an RDMSR
    when NMIs come from user mode.
    
    Fixes CVE-2015-3290.
    
    Cc: stable@vger.kernel.org
    Reviewed-by: Steven Rostedt <rostedt@goodmis.org>
    Reviewed-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Andy Lutomirski <luto@kernel.org>
    [apw@canonical.com: fix 64bit build.]
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- arch/x86/include/asm/thread_info.h
+++ arch/x86/include/asm/thread_info.h
@@ -197,6 +197,10 @@ static inline unsigned long current_stack_pointer(void)
 
 #else /* !__ASSEMBLY__ */
 
+#ifdef CONFIG_X86_64
+# define cpu_current_top_of_stack (cpu_tss + TSS_sp0)
+#endif
+
 /* Load thread_info address into "reg" */
 #define GET_THREAD_INFO(reg) \
 	_ASM_MOV PER_CPU_VAR(kernel_stack),reg ; \
