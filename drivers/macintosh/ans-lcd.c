b174a620d6ab1f85135840ed325ff68604765be8
Tim Gardner <tim.gardner@canonical.com>
2015-07-20 10:37:30 -0600

    UBUNTU: SAUCE: Fix FTBS due to missing include
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/macintosh/ans-lcd.c
+++ drivers/macintosh/ans-lcd.c
@@ -5,6 +5,7 @@
 #include <linux/types.h>
 #include <linux/errno.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/miscdevice.h>
 #include <linux/fcntl.h>
 #include <linux/init.h>
