b1a5dc54ceece01d6ee060ab2d3c9433b70e0cab
Andy Whitcroft <apw@canonical.com>
2015-07-30 16:50:19 +0100

    UBUNTU: SAUCE: AUFS
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- fs/file_table.c
+++ fs/file_table.c
@@ -146,6 +146,7 @@ over:
 	}
 	return ERR_PTR(-ENFILE);
 }
+EXPORT_SYMBOL(get_empty_filp);
 
 /**
  * alloc_file - allocate and initialize a 'struct file'
@@ -307,6 +308,7 @@ void put_filp(struct file *file)
 		file_free(file);
 	}
 }
+EXPORT_SYMBOL(put_filp);
 
 void __init files_init(unsigned long mempages)
 { 
